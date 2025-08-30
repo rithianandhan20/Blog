2ff233201cec2b724a6fa0cc04f55fe2044f7f7e
rithianandhan20 <rithianandhan202gmail.com>
2025-08-30 16:18:14 +0530

    Fix Render deploy by updating vite.config.js

--- fish-blog-frontend/vite.config.js
+++ fish-blog-frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  preview: {
+    allowedHosts: ['aquatic-j7rc.onrender.com'], // allow Render domain
+    host: '0.0.0.0',                            // listen on all IPs
+    port: process.env.PORT || 4173,             // use Render's port
+  },
 })
