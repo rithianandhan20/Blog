19ab09f508bb42e6bec5fcaa727381fd18ee20ab
rithianandhan20 <rithianandhan202gmail.com>
2025-08-30 19:21:54 +0530

    xyz

--- fish-blog-frontend/src/components/BlogForm.jsx
+++ fish-blog-frontend/src/components/BlogForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BACKEND_URL = 'https://rithi.onrender.com/api';
+const BACKEND_URL = 'https://rithi.onrender.com/api/blogs';
 
 
 export default function BlogForm() {
